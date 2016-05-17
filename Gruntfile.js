9b19d3bc8b450f52cae78967a989234bc1bf19ed
crow <yangchen@quicloud.cn>
2016-05-17 18:57:12 +0800

    v0.1.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function (grunt) {
                     separator: '\n'
                 },
                 files: {
-                    'tmp/': 'modules/**'
+                    'tmp/': '/modules/**'
                 }
             }
         },
